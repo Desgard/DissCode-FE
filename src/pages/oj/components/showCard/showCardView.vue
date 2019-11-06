330746b17bd9e7f7aaa229fc5fc0fda660c31c33
Harry Twan <gua@desgard.com>
2019-11-06 08:34:04 +0800

    update about card view
    Signed-off-by: Harry Twan <gua@desgard.com>

---
+++ src/pages/oj/components/showCard/showCardView.vue
@@ -0,0 +1,35 @@
+<template>
+  <panel class="container">
+    <div slot="title">{title}</div>
+  </panel>
+</template>
+
+<script>
+
+export default {
+  name: 'ShowCardView',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .container {
+    margin-bottom: 20px;
+
+    .content {
+      font-size: 16px;
+      margin: 0 50px 20px 50px;
+      > ul {
+        list-style: disc;
+        li {
+          line-height: 2;
+          .title {
+            font-weight: 500;
+          }
+        }
+      }
+    }
+  }
+</style>
